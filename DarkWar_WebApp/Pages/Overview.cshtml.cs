04127c6193e222b16ca4943cecf034d2002e624a
Th3Molo <m.schinski@yahoo.com>
2025-07-29 19:57:20 +0200

    Add Table, Login

--- DarkWar_WebApp/Pages/Overview.cshtml.cs
+++ DarkWar_WebApp/Pages/Overview.cshtml.cs
@@ -5,8 +5,18 @@ namespace DarkWar_WebApp.Pages
 {
     public class OverviewModel : PageModel
     {
+        public List<Player> PlayerList { get; set; } = new List<Player>();
         public void OnGet()
         {
+            PlayerList.Add(new Player()
+            {
+                PlayerName = "Wuwu13",
+                CP = 135500000,
+                Rank = Rank.R5,
+                WatchtowerLevel = 30,
+                Events = new(),
+                Violationlist = new(),
+            });
         }
     }
 }
